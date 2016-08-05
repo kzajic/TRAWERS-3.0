0b8fde68fce90c10d125fbb94d5d8765eac68e98
kzajic <k.zajic@outlook.com>
2016-08-05 22:17:12 +0200

    web subpage_gazeta

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function (){
   
   
+  //menu animacja
+  
   var menuElements = $(".menu").find("li");
   
   menuElements.on('mouseenter', function(){ 
@@ -13,4 +15,44 @@ $(document).ready(function (){
   $(this).css("color", "#532B13");  
   });
   
+  
+  // zdjecia animacja
+  
+ var box = $(".main_box").find("nav").find(".box");
+  
+  box.on('mouseenter', function(){ 
+   $(this).css("background-size", "130% 180%");
+  $(this).find(".box_border").css("opacity", "1");
+  });
+  
+  box.on('mouseleave', function(){ 
+   $(this).css("background-size", "110% 160%");
+  $(this).find(".box_border").css("opacity", "0.6");
+  });
+  
+  
+    // zdjecia boxow z tekstem
+  
+var box_fest = $(".right_top");
+  
+  box_fest.on('mouseenter', function(){ 
+  $(this).find(".box_border").addClass("box_borderFest");
+   }); 
+  
+  box_fest.on('mouseleave', function(){ 
+  $(this).find(".box_border").removeClass("box_borderFest"); 
+   }); 
+  
+ var box_slajd = $(".right_bottom");
+  
+  box_slajd.on('mouseenter', function(){ 
+  $(this).find(".box_border").addClass("box_borderSlajd");
+   }); 
+  
+  box_slajd.on('mouseleave', function(){ 
+  $(this).find(".box_border").removeClass("box_borderSlajd"); 
+   });  
+  
 });
+
+
